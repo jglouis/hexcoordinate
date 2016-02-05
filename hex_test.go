b47658113752e4ed91321d402a011751fd96b1f6
Jean-Guillaume Louis <jgl@spaceapplications.com>
2016-02-05 13:15:29 +0100

    Add another test case for rotation

--- hex_test.go
+++ hex_test.go
@@ -5,20 +5,23 @@ import "testing"
 func TestAddition(t *testing.T) {
 	v1 := Vector{0, 1}
 	v2 := Vector{2, 3}
-	expected := Vector{2, 4}
+	want := Vector{2, 4}
 	got := Add(v1, v2)
 
-	if got != expected {
-		t.Errorf("Add %s and %s, got %s expected %s", v1, v2, got, expected)
+	if got != want {
+		t.Errorf("Add %s and %s, got %s expected %s", v1, v2, got, want)
 	}
 }
 
 func TestRotation(t *testing.T) {
-	o := N
-	got := o.Rotate(7)
-	expected := NE
+	tests := map[Orientation]Orientation{
+		N.Rotate(7):  NE,
+		S.Rotate(-1): SE,
+	}
 
-	if got != expected {
-		t.Errorf("got %s expected %s", got, expected)
+	for got, want := range tests {
+		if got != want {
+			t.Errorf("got %s but want %s", got, want)
+		}
 	}
 }
