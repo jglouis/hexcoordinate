3246ea2ddecc15c4845d2e0a3f1c87e99452b28c
Jean-Guillaume Louis <jgl@spaceapplications.com>
2016-02-05 12:19:03 +0100

    Add String method for hex.Vector

--- hex.go
+++ hex.go
@@ -1,8 +1,14 @@
 package hex
 
+import "fmt"
+
 // Vector is hexagonal vector with (U,V) axial coordinates
 type Vector struct {
 	U, V int
+}
+
+func (v Vector) String() string {
+	return fmt.Sprintf("Vector(%d,%d)", v.U, v.V)
 }
 
 // Add computes the sum vector
--- hex_test.go
+++ hex_test.go
@@ -6,8 +6,9 @@ func TestAddition(t *testing.T) {
 	v1 := Vector{0, 1}
 	v2 := Vector{2, 3}
 	expected := Vector{2, 4}
+	got := Add(v1, v2)
 
-	if Add(v1, v2) != expected {
-		t.Error()
+	if got != expected {
+		t.Errorf("Add %s and %s, got %s expected %s", v1, v2, got, expected)
 	}
 }
